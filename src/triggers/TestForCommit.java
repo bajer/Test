22de753a69d162f6451385622797b40cab0fdd70
Wojtek <Wojtek@WOJ-PC>
2014-03-10 19:17:26 +0000

    TestCOmmiut

---
+++ src/triggers/TestForCommit.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package triggers;
+
+/**
+ *
+ * @author Wojtek
+ */
+public class TestForCommit {
+    
+}
